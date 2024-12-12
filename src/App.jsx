f85d1d8b246ac21c778db07579eabfa580d9f124
ashutoshupadhyay23 <upadhyayashutosh423@gmail.com>
2024-12-12 14:41:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <div className="App">
+     <h1>Hi There!</h1>
+    </div>
+  )
+}
+
+export default App
